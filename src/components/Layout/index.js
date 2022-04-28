a40fba04299ba67b43ece3c0bae5944dfa5e031c
devbiran <ib660987@gmail.com>
2022-04-28 13:39:16 -0700

    fix build issue

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,7 @@ import { Box, Container } from '@mui/material';
 
 // Components
 import Header from '../Header';
-import LeftSideBar from '../LeftSideBar';
+import LeftSideBar from '../LeftSidebar';
 
 // Images
 import bgImage from '../../assets/images/bg-img.png';
